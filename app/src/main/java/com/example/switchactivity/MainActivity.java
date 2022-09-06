8773c8595f20524ffe9ef667a3c362957cd8cedc
HOANG THANG <thaithang115@gmail.com>
2022-09-06 08:52:26 +0700

    done

---
+++ app/src/main/java/com/example/switchactivity/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.switchactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_pana;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_pana = (Button) findViewById(R.id.button);
+        btn_pana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent panama = new Intent(MainActivity.this, porschepanamera.class);
+                startActivity(panama);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/switchactivity/porschepanamera.java
@@ -0,0 +1,14 @@
+package com.example.switchactivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class porschepanamera extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.porschepanamera);
+    }
+}
